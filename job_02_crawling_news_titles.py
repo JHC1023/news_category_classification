7e6cd3cd51612b5813940a663859642bf18cb9eb
reon96 <rlarudals_96@naver.com>
2025-04-16 15:52:04 +0900

    Add Crawling headlines of naver news by selenium

--- job_02_crawling_news_titles.py
+++ job_02_crawling_news_titles.py
@@ -1,9 +1,18 @@
+# raw_data = news_headlines.csv
+# columns = [title, category]
+# jinwo 0,1 / heachan 2,3 / gyeongmin 4,5
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import pandas as pd
+import datetime
+
+category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
+df_titles = pd.DataFrame()
 
 options = ChromeOptions()
 options.add_argument('lang=ko_KR')
@@ -11,21 +20,37 @@ options.add_argument('lang=ko_KR')
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
-url = 'https://news.naver.com/section/100'
-driver.get(url)
+# change int value by one's part
+my_category = [0, 0]
 button_xpath = '//*[@id="newsct"]/div[4]/div/div[2]'
 
-for cnt in range(15):
-    time.sleep(0.5)
-    driver.find_element(By.XPATH, button_xpath).click()
-time.sleep(5)
+for num in my_category:
+    url = 'https://news.naver.com/section/10{}'.format(num)
+    driver.get(url)
 
+    for cnt in range(15):
+        time.sleep(1)
+        try :
+            driver.find_element(By.XPATH, button_xpath).click()
+        except :
+            print("error of not more contents")
 
-for i in range(1, 6):
-    for j in range(1, 7):
-        title_path = '//*[@id="newsct"]/div[4]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(i, j)
-        try:
-            title = driver.find_element(By.XPATH, title_path).text
-            print(title)
-        except:
-            print('error', i, j)
+    time.sleep(5)
+    titles = []
+    for i in range(1, 100):
+        for j in range(1, 7):
+            title_path = '//*[@id="newsct"]/div[4]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(i, j)
+            try:
+                title = driver.find_element(By.XPATH, title_path).text
+                # print(title)
+                titles.append(title)
+            except:
+                print('error', i, j)
+    df_section_titles = pd.DataFrame(titles, columns=['titles'])
+    df_titles_category = pd.DataFrame(titles, columns=['titles'])
+
+    df_titles_category['category'] = category[num]
+    df_titles = pd.concat([df_titles, df_titles_category], axis=0, ignore_index=True)
+
+    df_titles.to_csv('./crawling_data/news_headlines_{}_{}.csv'.format(
+        category[num] , datetime.datetime.now().strftime('%Y%m%d')), index=False)
